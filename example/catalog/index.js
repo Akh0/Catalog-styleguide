bbc376f8cc95577a66e75ac3a012accebe306193
MACAW\Rocky <rocky.tjon.a.sin@macaw.nl>
2018-03-16 13:00:38 +0100

    Added card component

--- example/catalog/index.js
+++ example/catalog/index.js
@@ -7,7 +7,8 @@ const pages = [
     {path: '/color-palette', title: 'Color Palette', component: require('./pages/COLOR-PALETTE.md')},
     {path: '/typography', title: 'Typography', component: require('./pages/TYPOGRAPHY.md')},
     {path: '/grid', title: 'Grid', component: require('./pages/GRID.md')},
-    {path: '/buttons', title: 'Buttons', component: require('./pages/BUTTONS.md')}
+    {path: '/buttons', title: 'Buttons', component: require('./pages/BUTTONS.md')},
+    {path: '/card', title: 'Card', component: require('./pages/CARD.md')}
 ];
 
 ReactDOM.render(
